bfcf86d481e860a8bbb4516e542b5451a9514ead
Your Name <2429013@kiit.ac.in>
2026-06-14 23:58:06 +0530

    feat: add react-router-dom for routing and update registration component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import Register from "./auth/register"; 
-function App() { 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./auth/register";           
+// import Login from "./auth/login";
+ function App() { 
   return (  
-    <> 
-<Register />    
-</>
-    );
-}
-
-export default App;
+     <> 
+ <Register />    
+ </>           
+     );
+} 
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Register />} />
+//         <Route path="/login" element={<Login />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+export default App; 
